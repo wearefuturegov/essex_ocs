97209f891c4efce65f8c3f0a1c63cda0167af392
DomFreeman <dominic.freeman123@gmail.com>
2019-02-18 17:51:51 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     resources :steps, only: :show
   end
 
-  resources :responses, only: %i[create update show]
+  resources :responses, only: %i[create update show] do
+    patch :send_results_sms
+  end
 
   comfy_route :cms_admin, path: "/admin"
   # Ensure that this route is defined last
