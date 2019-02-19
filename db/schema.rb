bb08ada0d707794e957eb9c4428c0b1681fc6104
DomFreeman <dominic.freeman123@gmail.com>
2019-02-19 13:00:57 +0000

    Update schema for name response migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_153231) do
+ActiveRecord::Schema.define(version: 2019_02_19_113646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,6 +164,8 @@ ActiveRecord::Schema.define(version: 2019_02_13_153231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone_number"
+    t.string "forename"
+    t.string "surname"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
