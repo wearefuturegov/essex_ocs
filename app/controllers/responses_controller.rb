7e01e25b0ebb688b95b0e4422394aec74d9a069b
DomFreeman <dominic.freeman123@gmail.com>
2019-02-19 17:41:20 +0000

    Add validation for name

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -6,9 +6,13 @@ class ResponsesController < ApplicationController
   end
 
   def create
-    response = Response.create(response_params)
-    session[:response_id] = response.id
-    redirect_to journeys_path
+    @response = Response.new(response_params)
+    if @response.save
+      session[:response_id] = @response.id
+      redirect_to journeys_path
+    else
+      render :new
+    end
   end
 
   def update
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,2 +1,4 @@
 class Response < ApplicationRecord
+  validates :surname, presence: true
+  validates :forename, presence: true
 end
