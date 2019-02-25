b0548773adf9a0357d0144be42ca5db0aa001cf7
Chris Evans <evanschris@users.noreply.github.com>
2019-02-25 14:34:40 +0000

    correct steps

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -3,8 +3,8 @@ $(function() {
   $('.display_hidden_radio').each(function() { $(this).displayHiddenRadio() });
   $('.display_hidden_checkbox').each(function() { $(this).displayHiddenCheckbox() });
   $('#firstQuestion').blankValidation('radio');
-  $('#stepQuestion_2').blankValidation('checkbox');
   $('#stepQuestion_3').blankValidation('checkbox');
+  $('#stepQuestion_4').blankValidation('checkbox');
 });
 
 
