4e6ffbe16fe2f979f7c75711f6febda33a1530e2
Chris Evans <evanschris@users.noreply.github.com>
2019-02-25 13:47:27 +0000

    wip for validation

--- app/assets/javascripts/fields.js
+++ app/assets/javascripts/fields.js
@@ -7,5 +7,18 @@ $.fn.displayHidden = function() {
       $('.hidden_field').hide().val('');
     }
   });
+};
+
+$.fn.blankValidation = function() {
+  var form = this.find($('form'));
+  form.submit(function(e) {
+    e.preventDefault();
+    $('input[type=radio]').each(function () {
+      if($(this).is(':checked')){
+
+      }
+    });
+    $('.form_errors').removeClass('display_none');
+  });
 
 };
--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,6 +1,7 @@
 $(function() {
   $('.accordion').each(function() { $(this).accordion() });
   $('.display_hidden').each(function() { $(this).displayHidden() });
+  $('#firstQuestion').blankValidation();
 });
 
 
