519a394a2edf8518b0ad4daf9316403960a3bfe3
DomFreeman <dominic.freeman123@gmail.com>
2019-03-11 11:10:21 +0000

    Replace name with participant number in permitted params. Remove name from text message

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -43,7 +43,7 @@ class ResponsesController < ApplicationController
   def send_results_sms
     @response = Response.find(params[:response_id])
     @response.update(response_params)
-    body = "Hello #{@response.forename}. You asked us to text some recommendations from the Home and healthy assistant. Just click on the link in this message to see what info and services we've found for you. https://www.homehealthy.co.uk/responses/#{@response.id}?sms=true"
+    body = "Hello, you asked us to text some recommendations from the Home and healthy assistant. Just click on the link in this message to see what info and services we've found for you. https://www.homehealthy.co.uk/responses/#{@response.id}?sms=true"
     TwilioService.send_sms(body, @response.phone_number)
     redirect_to response_path(@response)
   end
@@ -53,8 +53,7 @@ class ResponsesController < ApplicationController
   def response_params
     params.require(:response).permit(
       :phone_number,
-      :forename,
-      :surname,
+      :participant_number,
       :suggested_category,
       :suggested_help_category,
       :suggested_info_type,
