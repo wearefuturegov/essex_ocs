bc6f51421a06f14a208220f7209ea4243fe47ee7
DomFreeman <dominic.freeman123@gmail.com>
2019-04-11 15:00:41 +0100

    Update routes so that before action can be used to find response using id

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   resources :responses, only: %i[new create update show] do
     patch :send_results_sms
-    get '/:help_category_result', to: 'responses#help_category_result', as: 'help_category_result'
+    member do
+      get '/:help_category_result', to: 'responses#help_category_result', as: 'help_category_result'
+    end
   end
 
   comfy_route :cms_admin, path: "/admin"
