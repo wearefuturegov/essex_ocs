8d3882431f04932eab2f0371c85e13b6368f1f48
DomFreeman <dominic.freeman123@gmail.com>
2019-04-12 10:56:47 +0100

    Pass response when going back to first step to retain answer

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,7 +1,11 @@
 class JourneysController < ApplicationController
   def index
     @questions = questions
-    @response = Response.new
+    if params[:response_id] # If coming back, load response and previous answer
+      @response = Response.find(params[:response_id])
+    else
+      @response = Response.new
+    end
     @next_step = 1.to_i
   end
 end
